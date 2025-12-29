f4158b25c2f48f0dee59d4bc3c9f964acbf8ef8f
farhaan <farukiadnan45@student.sfit.ac.in>
2025-12-29 21:14:10 +0530

    updated

--- pages/admin/fabrics/[fabricId].js
+++ pages/admin/fabrics/[fabricId].js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import api from './utils/api';
+import api from '../../utils/api';
 
 export default function FabricDetails() {
   const router = useRouter();
